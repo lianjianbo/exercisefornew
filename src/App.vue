de7b22c6eab9ef1572f409a1171c8a6fa36106d4
unknown <986190866@qq.com>
2019-07-04 09:51:10 +0800

    完成搭建 vue 脚手架

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+   <ul>
+     <li>组播扫描</li>
+     <li>指定扫描</li>
+     <button>开始扫描</button>
+   </ul>
+   
+ </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+ 
+</style>
