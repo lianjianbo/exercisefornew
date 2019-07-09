1201679967248893bcc9d37d0dd7abd070fcccdf
chzhou <982708645@qq.com>
2019-07-09 18:34:04 +0800

    完成了第一个需求

--- README.md
+++ README.md
@@ -1,113 +1,30 @@
-# 产品需求与接口
+# dao
 
-## 需求
+> A Vue.js project
 
-![](./prototype.png)
+## Build Setup
 
-1. 进入页面：
-  - 与 mqtt broker 建立连接
-  - 订阅设备发现通知主题
-2. 退出页面：
-  - 与 mqtt broker 断开连接
-  - 退订设备发现通知主题
-3. 点击“开始扫描”：
-  - 构造相关请求，发送给服务器，如果成功则按钮文字变为“停止扫描”
-  - 接收设备发现通知，添加已发现的设备到页面列表
-4. 点击“停止扫描”，构造相关请求，发送给服务器，如果成功则按钮文字变为“开始扫描”
-5. 点击设备列表某一项
-  - 侧边栏显示相应设备的数据
-  - 点击侧边栏“设置”，构造提交数据请求，发送给服务器，如果成功则更新设备数据并提示“设置成功”，失败则提示“设置失败”
-6. 设备列表应分页渲染，每页 20 项
-7. 页面使用 vue-cli + webpack + element-ui 开发，web 请求使用 axios 发送和接收
+``` bash
+# install dependencies
+npm install
 
-## 接口
+# serve with hot reload at localhost:8080
+npm run dev
 
-1. mqtt broker
-  - 地址：`192.168.110.12`
-  - 端口：`8083`
-2. 设备发现通知：
-  - 主题：`/voerkadev/0/broadcast/discovering/device`
-  - 消息格式：
-```js
-{
-  "type" : 10,
-  "sid" : 0,
-  "tid" : 0,
-  "from" : "408d5c9bd7cd",
-  "to" : "",
-  "flags" : { },
-  "timestamp" : 1562133595113,
-  "payload" : {
-    "Header" : "MEEYI",
-    "SN" : "100000000001",  // 序列号
-    "Type" : "nshost",  // 类型
-    "Model" : "MockDev:192.168.1.190",  // 型号
-    "Version" : "V1.0.1",  // 固件版本
-    "Domain" : "hispro",
-    "MQTT" : "mqtt://mqtt.huanyutong.com:1883",
-    "MQTT_Username" : "",
-    "MQTT_Password" : "",
-    "Networks" : [ {
-      "Interface" : "eth1",
-      "MAC" : "C1:8F:1A:69:23:5A",  // MAC 地址
-      "DHCP" : 0,  // dhcp
-      "IP" : "192.168.0.3",  // ip 地址
-      "SubnetMask" : "255.255.255.0",  // 子网掩码
-      "Gateway" : "192.168.0.1",  // 网关
-      "DNS_prefer" : "8.8.8.8",  // 首选 DNS
-      "DNS_alter" : "114.114.114.114"  // 备选 DNS
-    }, {
-      "Interface" : "eth0",
-      "MAC" : "C4:ED:3A:F4:62:04",
-      "DHCP" : 0,
-      "IP" : "192.168.0.4",
-      "SubnetMask" : "255.255.255.0",
-      "Gateway" : "192.168.0.1",
-      "DNS_prefer" : "8.8.8.8",
-      "DNS_alter" : "114.114.114.114"
-    } ],
-    "WIFI_AP" : "",
-    "WIFI_Enable" : 0,
-    "WIFI_Secret" : 0,
-    "WIFI_Password" : "",
-    "Location" : "VIP",
-    "Long" : 0,
-    "Lati" : 0,
-    "ConfigPort" : 2887,
-    "Source" : "MULTICAST",
-    "Authorized" : true
-  }
-}
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
 ```
-3. 开始扫描请求：
-  - 请求：`GET http://192.168.118.190:9001/api/devices/search/start`
-  - 响应：
-```js
-{
-    "status": "success",  // 成功是 success；失败是 failed
-    "message": "用作提示文本",
-    "payload": {}
-}
-```
-4. 停止扫描请求：
-  - 请求：`GET http://192.168.118.190:9001/api/devices/search/stop`
-  - 响应：
-```js
-{
-    "status": "success",  // 成功是 success；失败是 failed
-    "message": "用作提示文本",
-    "payload": {}
-}
-```
-5. 设置设备请求：
-  - 请求：`PATCH http://192.168.118.190:9001/apps/hispro/api/devices/:设备序列号SN/attributes`
-  - 请求体：
-![](./request_body_for_device_attribute_patch.png)
-  - 响应：
-```js
-{
-    "status": "success",  // 成功是 success；失败是 failed
-    "message": "用作提示文本",
-    "payload": {}
-}
-```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/test1/index.vue
@@ -0,0 +1,266 @@
+<template>
+   <div>
+       <div id="first">
+        <el-radio v-model="radio" label="1">组播扫描</el-radio>
+        <el-radio v-model="radio" label="2">指定扫描</el-radio>
+        <el-button id="scan" onclick="myFunction()" type="primary">开始扫描</el-button>
+        </div>
+        <div>
+           <el-button type="primary" plain>设备认证</el-button>
+           <el-button type="danger">清空</el-button>
+           <el-select class="search" v-model="value" multiple  filterable remote reserve-keyword
+           placeholder="序列号/型号/IP" :remote-method="remoteMethod" :loading="loading">
+           <el-option
+           v-for="item in options"
+           :key="item.value"
+           :label="item.label"
+           :value="item.value">
+           </el-option>
+           </el-select>
+        </div>
+  <el-table
+       class="sn"
+    ref="multipleTable"
+    :data="tableData"
+    tooltip-effect="dark"
+    style="width: 5%"
+    @selection-change="handleSelectionChange">
+    <el-table-column
+      class="select"
+      type="selection"
+      height="90"
+      width="90">
+    </el-table-column>
+    </el-table>
+      <el-table
+      class="nav-get"
+    :data="tableData"
+    style="width: 70%"
+    :default-sort = "{prop: 'date', order: 'descending'}">
+    <el-table-column
+      label="序列号"
+      prop="SN"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="Type"
+      label="类型"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="Version"
+      label="固件版本"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="MAC"
+      label="MAC地址"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="DHCP"
+      label="DHCP"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="IP"
+      label="IP地址"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="SubnetMask"
+      label="子网掩码"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="Gateway"
+      label="网关"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="DNS"
+      label="DNS"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="Iden"
+      label="认证"
+      sortable
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="State"
+      label="状态"
+      sortable
+      width="120">
+    </el-table-column>
+    </el-table>
+    <div class="basicset">
+     <table ><i class="el-icon-d-arrow-right"></i>基本设置</table>
+    </div>
+    <div>
+    <ul>
+      <li class="list">序列号：</li>
+      <li class="list">MAC地址：</li>
+      <li class="list">固件版本：</li>
+    </ul>
+    </div>
+<div>
+  <el-switch class="swit"  v-model="value" inactive-text="DHCP"></el-switch>
+  <form class="tab">
+          <p>IP地址: <input type="text" name="fname" /></p>
+          <p>子网掩码: <input type="text" name="lname" /></p>
+          <p>网关: <input type="text" name="lname" /></p>
+          <p>首选DNS: <input type="text" name="lname" /></p>
+          <p>备选DNS: <input type="text" name="lname" /></p>
+          <el-button type="primary" class="set">设置</el-button>
+        </form>
+        </div>
+        <div class="block">
+    <span class="demonstration"></span>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="20"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="40">
+    </el-pagination>
+  </div>
+   </div>
+</template>
+<script>
+var mqtt = require('mqtt')
+var client
+export default {
+  data () {
+    return {
+      client: null,
+      radio: '1',
+      value: false,
+      checked: true,
+      multipleSelection: [],
+      tableData: [{
+        SN: '100000000001',
+        Type: 'nohost',
+        Version: 'v1.0.1',
+        MAC: 'C1:8F:1A:69:23:5A',
+        IP: '192.168.114.115',
+        SubnetMask: '255.255.255.0',
+        Gateway: '192.168.0.1',
+        DNS: '8.8.8.8/114.114.114.114'},
+      {
+        SN: '100000000002',
+        Type: 'nohost',
+        Version: 'v1.0.1',
+        MAC: 'C1:8F:1A:69:23:5A',
+        IP: '192.168.114.115',
+        SubnetMask: '255.255.255.0',
+        Gateway: '192.168.0.1',
+        DNS: '8.8.8.8/114.114.114.114'
+      }],
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4,
+      methods: {
+        toggleSelection (rows) {
+          if (rows) {
+            rows.forEach(row => {
+              this.$refs.multipleTable.toggleRowSelection(row)
+            })
+          } else {
+            this.$refs.multipleTable.clearSelection()
+          }
+        },
+        handleSelectionChange (val) {
+          this.multipleSelection = val
+        },
+        handleSizeChange (val) {
+          console.log(`每页 ${val} 条`)
+        },
+        handleCurrentChange (val) {
+          console.log(`当前页: ${val}`)
+        }
+      }
+    }
+  },
+  created () {
+      // 编写 mqtt 连接逻辑
+    client = mqtt.connect('ws://192.168.110.12:8083/mqtt')
+      // 连接
+    client.on('connect', function () {
+      console.log('connected')
+      // 订阅
+      client.subscribe('/voerkadev/0/broadcast/discovering/device')
+      // 发布
+      client.publish('/voerkadev/0/broadcast/discovering/device', 'Hello mqtt')
+      console.log('hello mqtt')
+    })
+  },
+  beforeDestroy () {
+    client.unsubscribe('/voerkadev/0/broadcast/discovering/device')
+    // mqtt断开连接
+    client.end()
+  }
+}
+</script>
+<style scoped>
+#first{
+  text-align: center;
+  margin-left:-1000px;
+  font-size: 15px;
+}
+.search{
+text-align: center;
+  margin-left: 820px;
+}
+.el-icon-d-caret{
+  color: gray;
+}
+.basicset{
+  margin-top: -200px;
+  margin-left: 1390px;
+}
+.el-icon-d-arrow-right{
+  color: skyblue;
+}
+.sn{
+  margin-top: 55px;
+}
+.nav-get{
+  margin-left: 60px;
+  margin-top: -155px;
+}
+.set{
+  margin-right: 50px;
+}
+.list{
+  margin-left: 1700px;
+  list-style:none;
+}
+.tab{
+float: right;
+margin-right:150px;
+margin-top: 10%;
+text-align:right;
+position: relative;
+}
+.swit{
+  margin-left: 1530px;
+  margin-top:170px;
+  position: absolute;
+}
+.block{
+  margin-top: 500px;
+}
+</style>
